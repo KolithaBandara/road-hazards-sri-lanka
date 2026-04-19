aff77192e56e3d39818456f9d37a574ba093417b
Kolitha <gayanb.kolitha@gmail.com>
2026-04-19 13:03:25 +0530

    feat(database): hide mongodb connection string

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,13 @@
+// dotenv to bring data from .env file
+require("dotenv").config();
+const databaseConnectionString = process.env.MONGODB_URI;
+
 const mongoose = require("mongoose");
 const dns = require("dns");
 dns.setServers(["8.8.8.8", "8.8.8.8"]);
 
 const connectDB = async () => {
-  await mongoose.connect("<connection String>/dbname");
+  await mongoose.connect(`${databaseConnectionString}`);
 };
 
 module.exports = connectDB;
