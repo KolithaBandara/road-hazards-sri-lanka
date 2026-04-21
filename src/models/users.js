632f3868b64e7272900ee5a97ae96e4a7908e212
Kolitha <gayanb.kolitha@gmail.com>
2026-04-21 19:07:20 +0530

    feat(utils): setup utils folder

--- src/models/users.js
+++ src/models/users.js
@@ -27,6 +27,11 @@ const userSchema = new Schema(
       required: true,
       unique: true,
     },
+    profilePic: {
+      type: String,
+      default:
+        "https://images.unsplash.com/photo-1494790108377-be9c29b29330?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
   },
   { timestamps: true },
 );
