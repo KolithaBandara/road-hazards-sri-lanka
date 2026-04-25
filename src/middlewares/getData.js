db4cf5ac166966435bd0f3f8e60fd7969851698c
Kolitha <gayanb.kolitha@gmail.com>
2026-04-25 11:55:40 +0530

    refactor(project): delete all project files

--- src/middlewares/getData.js
+++
@@ -1,9 +0,0 @@
-const getUserData = (req, res, next) => {
-  try {
-    throw new Error("Can't access data");
-  } catch (err) {
-    res.status(401).send("You can't get data... Contact the support team...!");
-  }
-};
-
-module.exports = { getUserData };
